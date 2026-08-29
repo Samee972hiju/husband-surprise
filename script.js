9e97103682bc0c50bf6c01c014dfa711f72a1358
Samee972hiju <zahrahiju81@gmail.com>
2026-08-29 18:40:07 +0500

    Update script.js

--- script.js
+++ script.js
@@ -4,15 +4,10 @@ document.getElementById("btn").onclick = function() {
   step++;
   
   if(step == 1) {
-    // Step 1: Video chalegi
     document.getElementById("surprise").innerHTML = 
-    `<video width="100%" autoplay loop>
+    `<video width="100%" controls autoplay>
       <source src="step1.mp4" type="video/mp4">
-      Tumhara browser video support nahi karta
     </video>`;
-  }
-  
-  if(step == 2) {
-    document.getElementById("surprise").innerHTML = "Step 2 baad me banayenge 💗";
+    document.getElementById("btn").innerText = "Dekh liya? 🥰";
   }
 }
