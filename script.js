c7934f11acb8ba271d6601ea2225546f70c40869
Samee972hiju <zahrahiju81@gmail.com>
2026-08-29 18:28:30 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 let step = 0;
-const messages = [
-  "Dudu 💗",
-  "Bubu 🩷", 
-  "I love you so much!",
-  "This is your surprise!"
-];
+
 document.getElementById("btn").onclick = function() {
-  document.getElementById("surprise").innerText = messages[step];
   step++;
+  
+  if(step == 1) {
+    // Step 1: Video chalegi
+    document.getElementById("surprise").innerHTML = 
+    `<video width="100%" autoplay loop>
+      <source src="step1.mp4" type="video/mp4">
+      Tumhara browser video support nahi karta
+    </video>`;
+  }
+  
+  if(step == 2) {
+    document.getElementById("surprise").innerHTML = "Step 2 baad me banayenge 💗";
+  }
 }
